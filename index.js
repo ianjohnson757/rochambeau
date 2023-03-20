08163f1c91c53cc30b329eb5ba2ebca1010df78e
Ian Johnson <ianjohnson757@gmail.com>
2023-03-20 12:55:05 -0400

    feat: going for round 2
    I finished building the game not happy with the outcome going for round 2

--- index.js
+++ index.js
@@ -1,49 +1,80 @@
-// rock paper scissors
-// the functions playerSelection vs computerSelection
-// random computer generated
-// 5 rounds
-// end of every round return a string like "You Lose! Paper beats Rock"
-const options = [`rock`, `paper`, `scissors`]
 
-function computerChoice(){
-// random input for computer
-    const choice = options[Math.floor(Math.random()* options.length)];
-    return choice
-}
-function checkWinner(playerSelection, computerSelection){
-    if(playerSelection == computerSelectionSelection){
-        return `Tie!`;
-    }
-    else if(
-        (playerSelection == `rock` && computerSelection == `scissors`) ||
-        (playerSelection == `scissors` && computerSelection == `paper`) ||
-        (playerSelection == `paper` && computerSelection == `rock`)
-    ){
-        return `Player`;
-    } 
-    else {
-        return `Computer`;
-    }   
+// First attempt Help from Dors YT
+// const options = [`rock`, `paper`, `scissors`]
 
-}
-console.log(checkWinner)
-function playRound(playerSelection, computerSelection){
+// function computerChoice(){
+//     const choice = options[Math.floor(Math.random()* options.length)];
+//     console.log(choice)
+//     return choice
+// }
 
 
-}
-computerChoice();
-// function game() {
-// // play
-// // five rounds
-// // console based
+// function checkWinner(playerSelection, computerSelection){
+//     if(playerSelection == computerSelection){
+//         return `Tie!`;  
+//     }
+//     else if(
+//         (playerSelection == `rock` && computerSelection == `scissors`) ||
+//         (playerSelection == `scissors` && computerSelection == `paper`) ||
+//         (playerSelection == `paper` && computerSelection == `rock`)
+//     ){
+//         return `Player`;
+//     } 
+//     else {
+//         return `Computer`;
+//     }    
 // }
 
 // function playRound(playerSelection, computerSelection){
-//     // end of every round return a string like "You Lose! Paper beats Rock"
-//     const playerSelection =
+//     const result = checkWinner(playerSelection, computerSelection);
+//     if(result == `Tie!`){
+//         return `It's a Tie!`
+//     }
+//     else if(result == `Player`){
+//         return `You Win! ${playerSelection} beats ${computerSelection}`
+//     }
+//     else{
+//         return `You Lose! Your ${playerSelection} was destroyed by ${computerSelection}`
+//     }
 // }
 
-// function playerChoice {
-// // input from player
+// function playerChoice(){
+//     let validatedInput = false;
+//     while(validatedInput == false){
+//         const choice = prompt(`Rock Paper Scissors`);
+//         if(choice == null){
+//             continue;
+//         }
+//         const choiceInLower = choice.toLowerCase();
+//         if(options.includes(choiceInLower)){
+//             validatedInput = true;
+//             return choiceInLower;
+//         }
+//     }
 // }
+
+// function game(){
+//     let scorePlayer = 0;
+//     let scoreComputer = 0;
+//     console.log(`Welcome!`)
+//     for (let i = 0; i <5; i++) {
+//         const playerSelection = playerChoice();
+//         const computerSelection = computerChoice();
+//         console.log(playRound(playerSelection, computerSelection));    
+//         if(checkWinner(playerSelection, computerSelection) == `Player`){
+//             scorePlayer++;
+//         } else if(checkWinner(playerSelection, computerSelection) == `Computer`){
+//             scoreComputer++;
+//         }  
+//     }
+//     console.log(`Game Over!`)
+//     if(scorePlayer > scoreComputer){
+//         console.log(`Winner!`)
+//     } else if(scorePlayer < scoreComputer){
+//         console.log(`Better Luck Next Time`)
+//     }
+// }
+
+// game()
+
 
